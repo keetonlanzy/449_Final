9a73b3e11b294cb20b3b90ef590da41a310f4930
lanzing1 <lanzing1@msu.edu>
2023-04-26 13:09:20 -0400

    Netlify working with supabase?

--- src/Navigation/Navbar.js
+++ src/Navigation/Navbar.js
@@ -3,8 +3,25 @@ import React from "react";
 import Accordion from 'react-bootstrap/Accordion';
 import KitchenSinkExample from "./KitchenSink";
 
-import { getServices } from './list';
-let val = getServices(); 
+import { createClient } from "https://cdn.skypack.dev/@supabase/supabase-js@2.8.0"
+
+const supabaseUrl = 'https://afzhzgaeufygfizasysm.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFmemh6Z2FldWZ5Z2ZpemFzeXNtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzc2MzA3OTUsImV4cCI6MTk5MzIwNjc5NX0.4BPg60lIeF2lM2CQ9ZfNnw0T5c1X86QP2KybKGUNgGI'
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+
+async function getServices() {
+  let { data: services, error } = await supabase
+      .from('services')
+      .select('*')
+
+  for (let service of services) {
+      let serviceList = document.getElementById('services');
+      serviceList.innerHTML += `<tr><td><td>${service.temp}  ${service.title}</td></tr>`;
+  }
+}
+
+getServices()
 
 
 
