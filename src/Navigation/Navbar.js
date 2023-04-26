096070dff63ad79130ac9327def93b52d36e94c6
lanzing1 <lanzing1@msu.edu>
2023-04-26 14:32:42 -0400

    added ;

--- src/Navigation/Navbar.js
+++ src/Navigation/Navbar.js
@@ -3,7 +3,7 @@ import React from "react";
 import Accordion from 'react-bootstrap/Accordion';
 import KitchenSinkExample from "./KitchenSink";
 
-import { createClient } from "https://cdn.skypack.dev/@supabase/supabase-js@2.8.0"
+import { createClient } from "https://cdn.skypack.dev/@supabase/supabase-js@2.8.0";
 
 const supabaseUrl = 'https://afzhzgaeufygfizasysm.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFmemh6Z2FldWZ5Z2ZpemFzeXNtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzc2MzA3OTUsImV4cCI6MTk5MzIwNjc5NX0.4BPg60lIeF2lM2CQ9ZfNnw0T5c1X86QP2KybKGUNgGI'
