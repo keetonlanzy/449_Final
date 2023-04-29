7480adde2239c21fe3ca984a2f1e73e4cbe92609
lanzing1 <lanzing1@msu.edu>
2023-04-28 21:02:36 -0400

    added js feature: back to top button

--- src/Navigation/Navbar.js
+++ src/Navigation/Navbar.js
@@ -7,7 +7,7 @@ import { createClient } from '@supabase/supabase-js'
 
 import Api from "../quotes";
 
-
+const toTop = () => window.scrollTo({top: 0, behavior: 'smooth'});
 
 const supabaseUrl = 'https://afzhzgaeufygfizasysm.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFmemh6Z2FldWZ5Z2ZpemFzeXNtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzc2MzA3OTUsImV4cCI6MTk5MzIwNjc5NX0.4BPg60lIeF2lM2CQ9ZfNnw0T5c1X86QP2KybKGUNgGI'
@@ -77,6 +77,11 @@ function BasicExample() {
       </Accordion.Item>
     </Accordion>
     </div>
+    
+    <div class="text-center">
+      <button className="btn btn-primary m-5" onClick={toTop}>Back to Top</button>
+    </div>
+
     </div>
   );
 }
--- src/quotes.js
+++ src/quotes.js
@@ -22,14 +22,14 @@ const Api = () => {
 
   const { content, author } = quotes;
   return (
-    <div className="box-centerside ms-5 me-5 mb-3 ps-5 pe-5 pt-2 pb-1 rounded-pill w-50" style={{background: 'lightGrey'}}>
+    <div className="box-centerside ms-5 me-5 mb-3 ps-5 pe-5 pt-5 pb-1 rounded-pill w-50" style={{background: 'lightGrey'}}>
       <div className="text">
         <p>{content}</p>
       </div>
       <div className="author text-end">
         <h5>{author}</h5>
         <div className="button-container text-center rounded-pill">
-          <button className="rounded-pill bg-primary-subtle" onClick={getNewQuote}>New Quote</button>
+          <button className="btn rounded-pill bg-primary-subtle" onClick={getNewQuote}>New Quote</button>
         </div>
       </div>
     </div>
@@ -37,3 +37,5 @@ const Api = () => {
 };
 
 export default Api;
+
+//api and general layout found here: https://github.com/kritika27/quotes-generator-react
